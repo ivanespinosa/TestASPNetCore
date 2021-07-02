ce93b4ea92957009896b598213bbf7ea49280531
Ivanus Espinosa <ivan.espinosaf@gmail.com>
2021-07-02 15:25:57 -0500

    first commit

---
+++ TestASPNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestASPNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
